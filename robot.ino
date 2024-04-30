a90493f8831b5046a0c105744c1f9ec01bef8279
Daniel Thurau <Daniel.N.Thurau@gmail.com>
2024-04-30 12:04:13 -0700

    Add a menu system for program selection and implement passBlunt and laser pointer programs

--- robot.ino
+++ robot.ino
@@ -1,30 +1,26 @@
 #include <Servo.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <U8glib.h>
 
 // OLED stuff
-#define OLED_RESET     4
-Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
+U8GLIB_SSD1306_128X64 display(U8G_I2C_OPT_NONE);
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-Servo servo_A0;//create servo object to control a servo A0
-Servo servo_A1;//create servo object to control a servo A1
-Servo servo_A2;//create servo object to control a servo A2
-Servo servo_A3;//create servo object to control a servo A3
-Servo servo_A6;//create servo object to control a servo A6
+Servo servo_A0;  //create servo object to control a servo A0
+Servo servo_A1;  //create servo object to control a servo A1
+Servo servo_A2;  //create servo object to control a servo A2
+Servo servo_A3;  //create servo object to control a servo A3
+Servo servo_A6;  //create servo object to control a servo A6
 
-int servo_A0_position = 0; // Start in the 0 degree position (facing forward)
-int servo_A1_position = 90; // Start in the 90 degree position (upright)
-int servo_A2_position = 90; // Start in the 90 degree position (upright)
-int servo_A3_position = 90; // Start in the 90 degree position (center)
-int servo_A6_position = 96; // Start in the 96 degree position (jaws closed)
+int servo_A0_position = 0;   // Start in the 0 degree position (facing forward)
+int servo_A1_position = 90;  // Start in the 90 degree position (upright)
+int servo_A2_position = 90;  // Start in the 90 degree position (upright)
+int servo_A3_position = 90;  // Start in the 90 degree position (center)
+int servo_A6_position = 96;  // Start in the 96 degree position (jaws closed)
 
-const int arduino_button = 4;     // the number of the pushbutton pin
-
-int PA6;
+const int arduino_button = 4;  // the number of the pushbutton pin
 
 bool PLAYER = true;
 
@@ -32,36 +28,37 @@ enum Program {
   NIL,
   PASS_BLUNT,
   LASER_POINTER,
+  CAT_TOY,
+  DANCE,
 };
-
 enum Program program = Program::NIL;
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
 
-
-  servo_A0.attach(9); //attachs the servo on pin 9 to servo object
-  servo_A1.attach(6); //attachs the servo on pin 6 to servo object
-  servo_A2.attach(5); //attachs the servo on pin 5 to servo object
-  servo_A3.attach(3); //attachs the servo on pin 3 to servo object
-  servo_A6.attach(11); //attachs the servo on pin 11 to servo object
+  servo_A0.attach(9);   //attachs the servo on pin 9 to servo object
+  servo_A1.attach(6);   //attachs the servo on pin 6 to servo object
+  servo_A2.attach(5);   //attachs the servo on pin 5 to servo object
+  servo_A3.attach(3);   //attachs the servo on pin 3 to servo object
+  servo_A6.attach(11);  //attachs the servo on pin 11 to servo object
 
   // Uncomment to see a silly robot dance.
   // startupDance();
 
+  // Reset to normalized motor positions
   resetPosition();
 
   // initialize the pushbutton pin as an input:
   pinMode(arduino_button, INPUT);
 
-  // setup the display 
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.setTextColor(WHITE);//Sets the font display color
-  display.clearDisplay();//cls
+  // initialize the display
+  attachDisplay();
 
   program = programMenu();
 }
+
+
 
 void loop() {
   switch (program) {
@@ -69,105 +66,168 @@ void loop() {
       printText("NiL");
       break;
     case 1:
-      printText("Pass blunt()");
+      passBluntLoop();
       break;
-    case 2: 
-      printText("Lase pointer()");
+    case 2:
+      laserPointerLoop();
+      break;
+    case 3:
+      printText("Cat Toy()");
+      break;
+    case 4:
+      printText("Dance Demo()");
       break;
   }
-  // displayServoPositions();
+}
+
+void attachDisplay() {
+  if (display.getMode() == U8G_MODE_R3G3B2)
+    display.setColorIndex(255);  // white
+  else if (display.getMode() == U8G_MODE_GRAY2BIT)
+    display.setColorIndex(3);  // max intensity
+  else if (display.getMode() == U8G_MODE_BW)
+    display.setColorIndex(1);  // pixel on
+  display.setFont(u8g_font_6x10);
+  display.setFontRefHeightExtendedText();
+  display.setDefaultForegroundColor();
+  display.setFontPosTop();
 }
 
 Program programMenu() {
+  char* menuItems[] = { "Blunt Rotation", "Laser Pointer", "Cat Toy", "Dance Demo" };
+  int numItems = sizeof(menuItems) / sizeof(menuItems[0]);
+
   while (true) {
-    displayMenu();
-    // if(digitalRead(arduino_button)==LOW){
-    //   while(digitalRead(arduino_button)==LOW){
-    //     delay(100);
-    //   }
+    displayMenu(menuItems, numItems);
 
-    // }
-  }
-  return Program::LASER_POINTER;
-}
-
-void displayMenu() {
-  char *strings[] = {"Blunt Rotation", "Laser Pointer", "Cat Toy", "Dance Demo"};
-  display.clearDisplay();//cls
-  //Set the font size
-  display.setTextSize(1);
-  //Set the display location
-  display.setCursor(0,0);
-  //String displayed
-  display.println("Select a program to run");
-  for (int i=0; i < 4; i++) {
-      display.println("%s", strings[i]);
-  }
-
-  //Began to show
-  display.display();
-}
-
-
-void passBlunt() {
-    if(digitalRead(arduino_button)==LOW){
-      while(digitalRead(arduino_button)==LOW){
+    // Select the program based on the button click and position of A0 potentiometer
+    if (digitalRead(arduino_button) == LOW) {
+      while (digitalRead(arduino_button) == LOW) {
         delay(100);
+        int selectedItem = menuSelector(menuItems, numItems);
+
+        switch (selectedItem) {
+          case 0:
+            return Program::PASS_BLUNT;
+          case 1:
+            return Program::LASER_POINTER;
+          case 2:
+            return Program::CAT_TOY;
+          case 3:
+            return Program::DANCE;
+        }
       }
-      PLAYER = !PLAYER;
-      moveToPositionDouble(servo_A1, servo_A2, 85, 85);
-      servo_A1_position = 90;
-      servo_A2_position = 90;
-      delay(1000);
-      if (PLAYER == 1) {
-        servo_A0_position = 0;
-        servo_A0.write(servo_A0_position);
-      } else {
-        servo_A0_position = 180;
-        servo_A0.write(servo_A0_position);
-      }
-      delay(1000);
-      moveToPositionDouble(servo_A1, servo_A2, 45, 45);
-      servo_A1_position = 45;
-      servo_A2_position = 45;
-      delay(1000);
     }
+  }
+
+  return Program::NIL;
+}
+
+void displayMenu(char* menuItems[], int numItems) {
+  int potentiometer_A0 = menuSelector(menuItems, numItems);
+
+  display.firstPage();
+  do {
+    display.setFont(u8g_font_6x10);  // Set the font
+
+    char title_buffer[32];
+    sprintf(title_buffer, "Select a program: ", numItems);
+    display.drawStr(0, 0, title_buffer);  // Draw the title at the top
+
+    for (int i = 0; i < numItems; i++) {
+      char buffer[32];
+      // Format each string with a leading item number
+      if (i == potentiometer_A0) {
+        sprintf(buffer, "-> %d. %s", i + 1, menuItems[i]);
+      } else {
+        sprintf(buffer, "%d. %s", i + 1, menuItems[i]);
+      }
+      // Draw each string, adjust y-coordinate accordingly
+      display.drawStr(0, 11 + i * 10, buffer);
+    }
+  } while (display.nextPage());
+}
+
+int menuSelector(char* menuItems[], int numItems) {
+  int potentiometer_A0 = map(analogRead(0), 0, 1023, 0, numItems - 1);
+  return potentiometer_A0;
+}
+
+void passBluntLoop() {
+  printText("Pass Blunt Program");
+  if (digitalRead(arduino_button) == LOW) {
+    while (digitalRead(arduino_button) == LOW) {
+      delay(100);
+    }
+    PLAYER = !PLAYER;
+    moveToPositionDouble(servo_A1, servo_A2, 85, 85);
+    servo_A1_position = 90;
+    servo_A2_position = 90;
+    delay(1000);
+    if (PLAYER == 1) {
+      servo_A0_position = 0;
+      servo_A0.write(servo_A0_position);
+    } else {
+      servo_A0_position = 180;
+      servo_A0.write(servo_A0_position);
+    }
+    delay(1000);
+    moveToPositionDouble(servo_A1, servo_A2, 45, 45);
+    servo_A1_position = 45;
+    servo_A2_position = 45;
+    delay(1000);
+  }
+}
+
+void laserPointerLoop() {
+  servo_A0_position = map(analogRead(0), 0, 1023, 0, 180);
+  servo_A0.write(servo_A0_position);
+  servo_A1_position = map(analogRead(1), 0, 1023, 0, 180);
+  servo_A1.write(servo_A1_position);
+  servo_A2_position = map(analogRead(2), 0, 1023, 0, 180);
+  servo_A2.write(servo_A2_position);
+  servo_A3_position = map(analogRead(3), 0, 1023, 0, 180);
+  servo_A3.write(servo_A3_position);
+  servo_A6_position = map(analogRead(6), 0, 1023, 35, 90);
+  servo_A6.write(servo_A6_position);
+  displayServoPositions();
 }
 
 
-void printText(String text) {
-  display.clearDisplay();//cls
-  //Set the font size
-  display.setTextSize(1);
-  //Set the display location
-  display.setCursor(0,0);
-  //String displayed
-  display.println(text);
-
-  //Began to show
-  display.display();
+void printText(const char* text) {
+  display.firstPage();
+  do {
+    display.setFont(u8g_font_6x10);  // Set the font
+    display.drawStr(0, 0, text);     // Draw the title at the top
+  } while (display.nextPage());
 }
 
 void displayServoPositions() {
-  display.clearDisplay();//cls
-  //Set the font size
-  display.setTextSize(1);
-  //Set the display location
-  display.setCursor(0,0);
-  //String displayed
-  display.println("Blunt Rotation");
-  display.print("A0: "); display.println(servo_A0.read());
-  display.print("A1: "); display.println(servo_A1.read());
-  display.print("A2: "); display.println(servo_A2.read());
-  display.print("A3: "); display.println(servo_A3.read());
-  display.print("A6: "); display.println(servo_A6.read());
+  char buffer[40];  // Buffer for formatted strings
 
-  display.print("PLAYER: "); display.println(PLAYER);
+  display.firstPage();  // Begin the drawing loop
+  do {
+    display.setFont(u8g_font_6x10);  // Set the font size
 
-  //Began to show
-  display.display();
+    // Draw servo position
+    sprintf(buffer, "A0: %d", servo_A0.read());
+    display.drawStr(0, 0, buffer);
 
+    sprintf(buffer, "A1: %d", servo_A1.read());
+    display.drawStr(0, 10, buffer);
+
+    sprintf(buffer, "A2: %d", servo_A2.read());
+    display.drawStr(0, 20, buffer);
+
+    sprintf(buffer, "A3: %d", servo_A3.read());
+    display.drawStr(0, 30, buffer);
+
+    sprintf(buffer, "A6: %d", servo_A6.read());
+    display.drawStr(0, 40, buffer);
+
+  } while (display.nextPage());  // Loop through all pages
 }
+
 
 void startupDance() {
   // Sets the Servos to their default postion (forward/upright/closed)
@@ -218,7 +278,6 @@ void resetPosition() {
   servo_A2_position = moveToPosition(servo_A2, 90);
   servo_A3_position = moveToPosition(servo_A3, 90);
   servo_A6_position = moveToPosition(servo_A6, 97);
-
 }
 
 void moveToPositionDouble(Servo servoA, Servo servoB, int positionA, int positionB) {
@@ -231,7 +290,6 @@ void moveToPositionDouble(Servo servoA, Servo servoB, int positionA, int positio
     delay(10);
   }
 }
-
 
 int moveToPosition(Servo servo, int position) {
   int cp = servo.read();
